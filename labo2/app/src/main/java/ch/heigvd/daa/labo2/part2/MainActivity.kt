9dc44b4f96093b81219eb37689cc550c094619d8
DrCookie <timothee.vanhove@heig-vd.ch>
2023-10-06 17:31:46 +0200

    Separated in 3 packages packages

---
+++ labo2/app/src/main/java/ch/heigvd/daa/labo2/part2/MainActivity.kt
@@ -0,0 +1,12 @@
+package ch.heigvd.daa.labo2.part2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ch.heigvd.daa.labo2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main3)
+    }
+}
---
+++ labo2/app/src/main/java/ch/heigvd/daa/labo2/part3/MainActivity.kt
@@ -0,0 +1,12 @@
+package ch.heigvd.daa.labo2.part3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ch.heigvd.daa.labo2.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
