f7ad895e464a514932dfaca2283213c754ca36d9
DrC0okie <timothee.vanhove@heig-vd.ch>
2023-10-09 21:49:58 +0200

    Almost finished part3

---
+++ labo2/app/src/main/java/ch/heigvd/daa/labo2/part3/Part3Fragment.kt
@@ -0,0 +1,48 @@
+package ch.heigvd.daa.labo2.part3
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import ch.heigvd.daa.labo2.R
+
+class Part3Fragment : Fragment() {
+
+    private var fragmentNumber: Int? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            fragmentNumber = it.getInt(ARG_FRAG_NBR)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_part3, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val contentText = view.findViewById<TextView>(R.id.text_view_content)
+        contentText.text = getString(R.string.fragment_number, fragmentNumber)
+    }
+
+    companion object {
+        private const val ARG_FRAG_NBR = "step"
+
+        @JvmStatic
+        fun newInstance(step: Int): Part3Fragment {
+            val fragment = Part3Fragment()
+            val args = Bundle().apply {
+                putInt(ARG_FRAG_NBR, step)
+            }
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
--- labo2/app/src/main/java/ch/heigvd/daa/labo2/part3/Part3MainActivity.kt
+++ labo2/app/src/main/java/ch/heigvd/daa/labo2/part3/Part3MainActivity.kt
@@ -2,11 +2,54 @@ package ch.heigvd.daa.labo2.part3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import ch.heigvd.daa.labo2.R
+import ch.heigvd.daa.labo2.databinding.ActivityMainPart3Binding
 
 class Part3MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainPart3Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_part3)
+
+        binding = ActivityMainPart3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Default fragment
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                add(R.id.fragment_container, Part3Fragment.newInstance(1))
+                addToBackStack(null)
+            }
+        }
+
+        binding.buttonBack.setOnClickListener { onBackClicked() }
+
+        binding.buttonClose.setOnClickListener { onCloseClicked() }
+
+        binding.buttonNext.setOnClickListener { onNextClicked() }
+
+    }
+
+    private fun onNextClicked() {
+        val currentBackStackCount = supportFragmentManager.backStackEntryCount
+        val nextFragment = Part3Fragment.newInstance(currentBackStackCount + 1)
+        supportFragmentManager.commit {
+            replace(R.id.fragment_container, nextFragment)
+            addToBackStack(null)
+        }
+    }
+
+    private fun onCloseClicked() {
+        finish()
+    }
+
+    private fun onBackClicked() {
+        if (supportFragmentManager.backStackEntryCount > 1) {
+            supportFragmentManager.popBackStack()
+        } else {
+            finish()
+        }
     }
 }
