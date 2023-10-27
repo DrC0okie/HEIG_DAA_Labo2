0b35e9ffaa113cc0cd7cd207b549a3f86e1b15c0
Felix Breval <felix.breval@heig-vd.ch>
2023-10-27 13:25:15 +0200

    part 1 done

--- labo2/app/src/main/java/ch/heigvd/daa/labo2/part1/EditActivity.kt
+++ labo2/app/src/main/java/ch/heigvd/daa/labo2/part1/EditActivity.kt
@@ -1,5 +1,6 @@
 package ch.heigvd.daa.labo2.part1
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -8,11 +9,10 @@ import androidx.appcompat.app.AppCompatActivity
 import ch.heigvd.daa.labo2.R
 
 class EditActivity: AppCompatActivity(){
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_part1)
-
-
         val inName = intent?.getStringExtra(EditName.IN_NAME)
         val saveButton = findViewById<Button>(R.id.button_save)
         val nameEditor = findViewById<EditText>(R.id.edit_text_name)
--- labo2/app/src/main/java/ch/heigvd/daa/labo2/part1/Part1MainActivity.kt
+++ labo2/app/src/main/java/ch/heigvd/daa/labo2/part1/Part1MainActivity.kt
@@ -1,52 +1,56 @@
 package ch.heigvd.daa.labo2.part1
 
-import android.content.Context
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.result.contract.ActivityResultContract
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import ch.heigvd.daa.labo2.R
+import androidx.appcompat.widget.AppCompatTextView
 
 class Part1MainActivity : AppCompatActivity() {
 
-    private val NAME_PARAM = "name"
+    private val ARG_NAME = "name"
 
     private var name: String = ""
 
-    val welcomeMessage = findViewById<androidx.appcompat.widget.AppCompatTextView>(R.id.text_view_welcome_message)
+    private lateinit var welcomeMessage: AppCompatTextView
 
-    val editButton = findViewById<androidx.appcompat.widget.AppCompatButton>(R.id.button_edit_name)
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putString(NAME_PARAM, name)
+        outState.putString(ARG_NAME, name)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        name = savedInstanceState.getString(NAME_PARAM, "")
+        name = savedInstanceState.getString(ARG_NAME, "")
         updateInterface()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_part1) // Set the layout
+
+        val editButton = findViewById<Button>(R.id.button_edit_name) // Initialize editButton
+        welcomeMessage = findViewById<AppCompatTextView>(R.id.text_view_welcome_message) // Initialize welcomeMessage
+
         editButton.setOnClickListener {
             editName.launch(name)
         }
+
         updateInterface()
     }
 
     private val editName = registerForActivityResult(EditName()) { newName ->
-        newName?.let{
+        newName?.let {
             name = it
         }
         updateInterface()
     }
 
     fun updateInterface() {
-        if(name.isEmpty())
+        if (name.isEmpty())
             welcomeMessage.setText(R.string.main_welcome_message_anonymous)
         else
-            welcomeMessage.setText(getString(R.string.main_welcome_message, name))
+            welcomeMessage.text = getString(R.string.main_welcome_message, name)
     }
-
 }
+
